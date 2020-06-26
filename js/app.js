ac614b9c194f7bab8b1ba988177d5d24aa9d0a89
Riyiike <odusanyaiyanuoluwa@gmail.com>
2020-06-26 01:01:56 -0700

    main health tips

--- js/app.js
+++ js/app.js
@@ -3,18 +3,15 @@
   const quotes = [
     {
       quote:
-        "Life is too short and sweet to be spent by cribbing and complaining about things. Here are some random quotes about the most wonderful gift that we've got",
-      author: " Life"
+      "Wash your hands often with soap and water for at least 20 seconds, especially after being in a public place, or after blowing your nose, coughing or sneezing."
     },
     {
       quote:
-        "Humor is richly rewarding to the person who employs it. It has some value in gaining and holding attention. But it has no persuasive value at all",
-      author: "John Kenneth Galbraith"
+        "If soap and water are not readily available, use a hand sanitizer with at least 60% alcohol."
     },
     {
       quote:
-        "God save me from my friends. I can protect myself from my enemies.",
-      author: "Claude Louis Hector de Villars "
+        "Avoid touching your eyes, nose and mouth with unwashed hands. "
     },
     {
       quote: "The price of anything is the amount of life you exchange for it.",
@@ -22,14 +19,32 @@
     },
     {
       quote:
-        "Life is like a landscape. You live in the midst of it but can describe it only from the vantage point of distance. ",
-      author: "Charles Lindbergh"
+        "Avoid close contact with people who are sick. Some people without symptoms may be able to spread the virus."
     },
     {
       quote:
-        "A critic is someone who never actually goes to the battle, yet who afterwards comes out shooting the wounded.",
-      author: " Tyne Daly"
-    }
+        "Practice social distancing by keeping at least 6 feet — about two arm lengths — away from others if you must go out in public."
+    },
+
+    {
+      quote:
+        "Stay home as much as possible and avoid non-essential travel."
+    },
+
+    {
+      quote:
+      "Cover your mouth and nose with a cloth face cover when around others and when you must go out in public, such as to a grocery store. The cloth face cover is meant to protect other people in case you are infected."
+    },
+
+    {
+      quote:
+      "Cover your coughs and sneezes. Use a tissue to cover your nose and mouth, and throw used tissues in a lined trash can. If a tissue isn’t available, cough or sneeze into your elbow — not your hands. Wash your hands immediately.."
+    },
+
+    {
+      quote:
+      "Clean and disinfect frequently touched surfaces daily. This includes tables, doorknobs, light switches, countertops, handles, desks, phones, keyboards, toilets, faucets and sinks. Follow CDC guidance."
+    }   
   ];
 
   const btn = document.getElementById("generate-btn");
@@ -39,6 +54,6 @@
     console.log(random);
 
     document.getElementById("quote").textContent = quotes[random].quote;
-    document.querySelector(".author").textContent = quotes[random].author;
+    
   });
 })();
